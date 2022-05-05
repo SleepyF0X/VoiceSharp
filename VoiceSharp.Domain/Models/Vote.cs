067bf6ee74b9dc38038901d583c05efa3cb3b46d
SleepyF0X <kotohomka@gmail.com>
2022-05-06 01:11:13 +0300

    Init Vue

---
+++ VoiceSharp.Domain/Models/Vote.cs
@@ -0,0 +1,10 @@
+﻿using VoiceSharp.Domain.Contracts;
+
+namespace VoiceSharp.Domain.Models;
+
+public class Vote : Entity, IWithDateCreated
+{
+    public string Name { get; set; }
+    public DateTimeOffset DateCreated { get; private set; }
+    void IWithDateCreated.SetDateCreated(DateTimeOffset value) => DateCreated = value;
+}
