86d8ce7f88361c416b50a058b90e7c6fdce1ac4b
SleepyF0X <kotohomka@gmail.com>
2022-05-11 01:12:21 +0300

    Add Sidebar, add packages

--- Client/voice-sharp/src/router/index.js
+++ Client/voice-sharp/src/router/index.js
@@ -1,11 +1,23 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "@/views/auth/LoginView";
+import RegisterView from "@/views/auth/RegisterView";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: LoginView,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterView,
   },
   {
     path: "/about",
